947ffc4c40a7188509266f21427545fcd2f55932
RuslanWorkGit <lulka758@gmail.com>
2024-12-28 22:48:15 +0200

    add info to table view PlaylistMoveDeleteViewControllerPlaylistMoveDeleteViewController

--- HomeWork10UK/HomeWork10UK/Source/Module/PlaylistMoveDeleteScreen/PlaylistMoveDeleteViewController.swift
+++ HomeWork10UK/HomeWork10UK/Source/Module/PlaylistMoveDeleteScreen/PlaylistMoveDeleteViewController.swift
@@ -15,10 +15,69 @@ class PlaylistMoveDeleteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupInitialState()
+        model.loadData()
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editMode))
     }
     
     private func setupInitialState() {
         
         model = PlaylistMoveDeleteModel()
+        
+        model.delegate = self
+        
+        contentView.delegate = self
+        
+        contentView.tableView.delegate = self
+        contentView.tableView.dataSource = self
     }
+                                                            
+                                                            
+    @objc func editMode() {
+        contentView.tableView.isEditing.toggle()
+        
+        navigationItem.rightBarButtonItem?.title = contentView.tableView.isEditing ? "Done" : "Edit"
+        }
+}
+
+extension PlaylistMoveDeleteViewController: PlaylistMoveDeleteModelDelegate {
+    func dataDidLoad() {
+        contentView.tableView.reloadData()
+    }
+    
+    
+}
+
+extension PlaylistMoveDeleteViewController: PlaylistMoveDeleteViewDelegate {
+    
+}
+
+
+
+extension PlaylistMoveDeleteViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        model.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainPlaylistCell") else {
+            assertionFailure()
+            return UITableViewCell()
+        }
+        
+        cell.detailTextLabel?.numberOfLines = 0
+        cell.textLabel?.text = model.items[indexPath.row].songTitle
+        cell.detailTextLabel?.text = "Author - \(model.items[indexPath.row].author)\n" +
+                                    "Almub - \(model.items[indexPath.row].albumTitle)\n" +
+                                    "Genre - \(model.items[indexPath.row].genre)"
+        
+        return cell
+    }
+    
+    
+}
+
+extension PlaylistMoveDeleteViewController: UITableViewDelegate {
+    
 }
