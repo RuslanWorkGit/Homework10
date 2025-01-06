d2af1abd8d4118e8a327f4370b601700f87d1ce5
RuslanWorkGit <lulka758@gmail.com>
2025-01-06 11:39:39 +0200

    done task for PlaylistMoveDeleteViewController
    add button on right navigation bar controller
    done task with deleting item and moving item

--- HomeWork10UK/HomeWork10UK/Source/Module/PlaylistMoveDeleteScreen/PlaylistMoveDeleteViewController.swift
+++ HomeWork10UK/HomeWork10UK/Source/Module/PlaylistMoveDeleteScreen/PlaylistMoveDeleteViewController.swift
@@ -19,7 +19,9 @@ class PlaylistMoveDeleteViewController: UIViewController {
         
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editMode))
+        
     }
+    
     
     private func setupInitialState() {
         
@@ -75,11 +77,32 @@ extension PlaylistMoveDeleteViewController: UITableViewDataSource {
         return cell
     }
     
+   
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            model.items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
  
+
     
     
 }
 
 extension PlaylistMoveDeleteViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
     
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let modelItem = model.items.remove(at: sourceIndexPath.row)
+        model.items.insert(modelItem, at: destinationIndexPath.row)
+    }
+
 }
